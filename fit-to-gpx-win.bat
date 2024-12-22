df5b16bbdb915c6911f1989c1fb126f4f8438b4e
marty <mc3k@users.noreply.github.com>
2024-12-22 11:04:44 +0000

    Add files via upload

---
+++ fit-to-gpx-win.bat
@@ -0,0 +1,4 @@
+@echo off
+Title Batch convert fit to gpx
+for %%i in (*.fit) do "C:\Program Files\GPSBabel\gpsbabel.exe" -t -i garmin_fit -f %%i -x track,course,speed -o gpx -F %%~ni.gpx && echo %%i converted to %%~ni.gpx
+pause
