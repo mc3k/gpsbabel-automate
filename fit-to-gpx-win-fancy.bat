aac3a191e9a8c92ec4848bd93e65e29717457af6
marty <mc3k@users.noreply.github.com>
2024-12-22 14:12:27 +0000

    Added route as well as track

--- fit-to-gpx-win-fancy.bat
+++ fit-to-gpx-win-fancy.bat
@@ -1,4 +1,4 @@
 @echo off
 Title Batch convert fit to gpx
-for %%i in (*.fit) do "C:\Program Files\GPSBabel\gpsbabel.exe" -t -i garmin_fit -f %%i -x track,course,speed -o gpx -F %%~ni.gpx && echo [32m%%i[0m converted to [35m%%~ni.gpx[33m
+for %%i in (*.fit) do "C:\Program Files\GPSBabel\gpsbabel.exe" -rt -i garmin_fit -f %%i -x track,course,speed -o gpx -F %%~ni.gpx && echo [32m%%i[0m converted to [35m%%~ni.gpx[33m
 pause
